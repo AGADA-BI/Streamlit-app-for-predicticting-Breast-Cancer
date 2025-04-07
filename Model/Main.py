839f4b2cfbf19db978641a29aa825459d5daec87
Joshua Agada <105236170+AGADA-BI@users.noreply.github.com>
2025-04-07 11:45:16 +0100

    first commit

---
+++ Model/Main.py
@@ -0,0 +1,6 @@
+import pandas as pd
+from sklearn.preprocessing import StandardScaler
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import accuracy_score, classification_report
+import pickle5 as pickle
